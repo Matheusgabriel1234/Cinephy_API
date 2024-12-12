eb168ee847c77e3024f22c2570478ad9a04ee2df
Matheusgabriel1234 <magaje2005@gmail.com>
2024-12-12 13:43:39 -0300

    Docker configuration

---
+++ src/main/java/projetos/test/Cinephy/CinephyApplication.java
@@ -0,0 +1,13 @@
+package projetos.test.Cinephy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinephyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinephyApplication.class, args);
+	}
+
+}
